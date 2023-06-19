32a99c4134bcadb839e1d2d084cdb6acebf67aab
naveenrenold <naveenrenold@gmail.com>
2023-06-19 09:16:35 +0530

    coommit

--- UI/perry/src/app/app-routing.module.ts
+++ UI/perry/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchComponent } from './Search/search/search.component';
+import { MainComponent } from './main/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'search',component:SearchComponent},{path:'',component:MainComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- UI/perry/src/app/app.module.ts
+++ UI/perry/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './Search/search/search.component';
+import { MainComponent } from './main/main/main.component';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({providedIn: 'root'})
+export class ServiceNameService {
+  constructor(private httpClient: HttpClient) { }
+
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
