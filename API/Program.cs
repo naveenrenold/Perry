8636587be23802f729924634fcd3e4e09ae53d4b
naveenrenold <naveenrenold@gmail.com>
2023-09-01 09:12:54 +0530

    Added cors

--- API/Program.cs
+++ API/Program.cs
@@ -6,12 +6,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddCors();
 
 
 
 var app = builder.Build();
-
+app.UseCors(x=>x.AllowAnyMethod().AllowAnyHeader().SetIsOriginAllowed(x=>true));
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
